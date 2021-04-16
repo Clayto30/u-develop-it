a216b9fb4eff14c0d91787ee3854f1569a556ab7
Clayton Goff <cltngoff@gmail.com>
2021-04-15 23:10:39 -0700

    added parties table

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
         // Your MyQL username
         user: 'root',
         // Your MySQL password
-        password: '',
+        password: 'footfart8',
         database: 'election'
     },
     console.log('Connected to the election database.')
