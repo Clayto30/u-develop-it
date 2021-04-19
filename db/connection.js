8bf1f143fec6256eb0677788b11f414942f6f95e
Clayton Goff <cltngoff@gmail.com>
2021-04-18 19:25:52 -0700

    final updates

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection(
         // Your MyQL username
         user: 'root',
         // Your MySQL password
-        password: 'footfart8',
+        password: 'password',
         database: 'election'
     });
 
